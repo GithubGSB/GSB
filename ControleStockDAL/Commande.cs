4a6dc9f454b7ac4d0e786d9c7d76f2e24253e57c
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-10 16:22:29 +0200

    Ajoutez des fichiers projet.

---
+++ ControleStockDAL/Commande.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockDAL
+{
+    /// <summary>
+    /// Contient un objet commande, qui permet d'effectué des requêtes de type procédure stocké sur la BD __nomBD
+    /// </summary>
+    public class Commande
+    {
+        private SqlCommand objCommande;
+
+        private static Commande uneInstance;
+        /// <summary>
+        /// Permet de récupéré l'instance de Commande
+        /// </summary>
+        /// <returns>instance de Commande</returns>
+        public static Commande GetInstance()
+        {
+            if (uneInstance == null) uneInstance = new Commande();
+            return uneInstance;
+        }
+        private Commande()
+        {
+            SqlConnection objConnex = new SqlConnection();
+            objConnex.ConnectionString = ConfigurationManager.ConnectionStrings[""].ConnectionString; //a completé
+            objCommande = objConnex.CreateCommand();
+            objCommande.CommandType = System.Data.CommandType.StoredProcedure;
+        }
+
+        /// <summary>
+        /// Ourverture d'une commande du la BD
+        /// </summary>
+        /// <returns>Objet responsable de la commande, ou null si imposible d'ouvrir une connexion</returns>
+        public SqlCommand GetObjCommande()
+        {
+            try
+            {
+                if (objCommande.Connection.State == System.Data.ConnectionState.Closed) objCommande.Connection.Open();
+                objCommande.Parameters.Clear();
+                return objCommande;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Fermeture de la connexion si elle est ouverte
+        /// </summary>
+        public void FermerConnexion()
+        {
+            if (objCommande.Connection != null && objCommande.Connection.State != System.Data.ConnectionState.Closed) objCommande.Connection.Close();
+        }
+    }
+}
---
+++ GSBControleStockage/FormAccueil.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace GSBControleStockage
+{
+    partial class FormAccueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuGSBControleStock = new System.Windows.Forms.MenuStrip();
+            this.mnuItemZoneStock = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuItemEntreprise = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuItemControleRealise = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuItemControleObliga = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuItemUtilisateur = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuGSBControleStock.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuGSBControleStock
+            // 
+            this.mnuGSBControleStock.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuGSBControleStock.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuItemZoneStock,
+            this.mnuItemEntreprise,
+            this.mnuItemControleRealise,
+            this.mnuItemControleObliga,
+            this.mnuItemUtilisateur});
+            this.mnuGSBControleStock.Location = new System.Drawing.Point(0, 0);
+            this.mnuGSBControleStock.Name = "mnuGSBControleStock";
+            this.mnuGSBControleStock.Size = new System.Drawing.Size(1087, 28);
+            this.mnuGSBControleStock.TabIndex = 0;
+            this.mnuGSBControleStock.Text = "menuStrip1";
+            // 
+            // mnuItemZoneStock
+            // 
+            this.mnuItemZoneStock.Name = "mnuItemZoneStock";
+            this.mnuItemZoneStock.Size = new System.Drawing.Size(205, 24);
+            this.mnuItemZoneStock.Text = "Gestion zones de stockages";
+            // 
+            // mnuItemEntreprise
+            // 
+            this.mnuItemEntreprise.Name = "mnuItemEntreprise";
+            this.mnuItemEntreprise.Size = new System.Drawing.Size(176, 24);
+            this.mnuItemEntreprise.Text = "Gestion des entreprises";
+            // 
+            // mnuItemControleRealise
+            // 
+            this.mnuItemControleRealise.Name = "mnuItemControleRealise";
+            this.mnuItemControleRealise.Size = new System.Drawing.Size(218, 24);
+            this.mnuItemControleRealise.Text = "Gestion des contrôles réalisés";
+            // 
+            // mnuItemControleObliga
+            // 
+            this.mnuItemControleObliga.Name = "mnuItemControleObliga";
+            this.mnuItemControleObliga.Size = new System.Drawing.Size(265, 24);
+            this.mnuItemControleObliga.Text = "Gestion des obligations de contrôles";
+            // 
+            // mnuItemUtilisateur
+            // 
+            this.mnuItemUtilisateur.Name = "mnuItemUtilisateur";
+            this.mnuItemUtilisateur.Size = new System.Drawing.Size(175, 24);
+            this.mnuItemUtilisateur.Text = "Gestion des utilisateurs";
+            // 
+            // FormAccueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 450);
+            this.Controls.Add(this.mnuGSBControleStock);
+            this.MainMenuStrip = this.mnuGSBControleStock;
+            this.Name = "FormAccueil";
+            this.Text = "GSB Contrôle des stockage";
+            this.mnuGSBControleStock.ResumeLayout(false);
+            this.mnuGSBControleStock.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuGSBControleStock;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemZoneStock;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemEntreprise;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemControleRealise;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemControleObliga;
+        private System.Windows.Forms.ToolStripMenuItem mnuItemUtilisateur;
+    }
+}
+
