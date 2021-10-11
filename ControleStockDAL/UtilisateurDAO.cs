65d1247933a581d393d6706b9e8dbe8ff7a830d9
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 17:28:32 +0200

    Ajout de la méthode ajoutUtilisateur

--- ControleStockDAL/UtilisateurDAO.cs
+++ ControleStockDAL/UtilisateurDAO.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ControleStockBO;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +23,36 @@ namespace ControleStockDAL
         {
             if (uneInstance == null) uneInstance = new UtilisateurDAO();
             return uneInstance;
+        }
+
+        /// <summary>
+        /// Permet d'ajouter un utilisateur dans la base de données
+        /// </summary>
+        /// <param name="nom">nom de l'utilisateur</param>
+        /// <param name="prenom">prénom de l'utilisateur</param>
+        /// <param name="motDePasse">mot de passe sécurisé de l'utilisateur</param>
+        /// <param name="sel">sel de sécurisation du mot du passe</param>
+        /// <param name="identifiantConnexion">identifiant de connexion de l'utilisateur</param>
+        /// <param name="profil">profil de l'utilisateur</param>
+        public void AjoutUtilisateur(string nom, string prenom, SecureString motDePasse, SecureString sel, string identifiantConnexion, Profil profil)
+        {
+            //récupération commande
+            SqlCommand cmd = Commande.GetInstance().GetObjCommande();
+            cmd.CommandText = "spAjoutUtilisateur";
+
+            //ajout des paramètres
+            cmd.Parameters.Add("nom", System.Data.SqlDbType.VarChar).Value = nom;
+            cmd.Parameters.Add("prenom", System.Data.SqlDbType.VarChar).Value = prenom;
+            cmd.Parameters.Add("motDePasse", System.Data.SqlDbType.VarChar).Value = motDePasse.FromSecureString();
+            cmd.Parameters.Add("selMotDePasse", System.Data.SqlDbType.VarChar).Value = sel.FromSecureString();
+            cmd.Parameters.Add("identifiantConnexion", System.Data.SqlDbType.VarChar).Value = identifiantConnexion;
+            cmd.Parameters.Add("idProfil", System.Data.SqlDbType.Int).Value = profil.Id;
+
+            //récupération des profils
+            cmd.ExecuteNonQuery();
+
+            //fermeture de la commande
+            Commande.GetInstance().FermerConnexion();
         }
     }
 }
