5e18877386c26814c56444af77c8dacfab3893e8
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 21:34:07 +0200

    ajout du manager utilisateur

---
+++ ControleStockBLL/UtilisateurManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockBLL
+{
+    /// <summary>
+    /// Classe permettant la gestion des utilisateurs
+    /// </summary>
+    public class UtilisateurManager
+    {
+        private static UtilisateurManager uneInstance;
+        /// <summary>
+        /// Premet de récupéré l'instance UtilisateurManager
+        /// </summary>
+        /// <returns>Instance de UtilisateurManager</returns>
+        public static UtilisateurManager GetInstance()
+        {
+            if (uneInstance == null) uneInstance = new UtilisateurManager();
+            return uneInstance;
+        }
+        private UtilisateurManager() { }
+    }
+}
