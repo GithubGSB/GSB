6911a53ba15e577cab09e32cf90387f0a1e967f7
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 17:16:12 +0200

    ajout methode getProfils

--- ControleStockDAL/ProfilDAO.cs
+++ ControleStockDAL/ProfilDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ControleStockBO;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,43 @@ namespace ControleStockDAL
         {
             if (uneInstance == null) uneInstance = new ProfilDAO();
             return uneInstance;
+        }
+
+        /// <summary>
+        /// Permet de récupéré tous les profils contenus dans la BD
+        /// </summary>
+        /// <returns>Liste des profils</returns>
+        public List<Profil> GetProfils()
+        {
+            //variable de travail
+            List<Profil> lesProfils = new List<Profil>();
+            string libelle, str;
+            int id;
+
+            //récupération commande
+            SqlCommand cmd = Commande.GetInstance().GetObjCommande();
+            cmd.CommandText = "spGetProfils";
+
+            //récupération des profils
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    //récupération données
+                    if (reader["libelle"] == DBNull.Value) libelle = default(string);
+                    else libelle = reader["libelle"].ToString();
+
+                    str = reader["id"].ToString();
+                    int.TryParse(str, out id);
+
+                    //ajout du profil
+                    lesProfils.Add(new Profil(id, libelle));
+                }
+            }
+
+            //fermeture de la commande
+            Commande.GetInstance().FermerConnexion();
+            return lesProfils;
         }
     }
 }
