401cb7b23bf36d79c1acb1f19c4b63e04ed3d035
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 17:18:19 +0200

    ajout de UtilisateurDAO

---
+++ ControleStockDAL/UtilisateurDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockDAL
+{
+    // <summary>
+    /// Classe permettant l'accès aux données relatif à l'utilisateur
+    /// </summary>
+    public class UtilisateurDAO
+    {
+        private static UtilisateurDAO uneInstance;
+        /// <summary>
+        /// Premet de récupéré l'instance UtilisateurDAO
+        /// </summary>
+        /// <returns>Instance de UtilisateurDAO</returns>
+        public static UtilisateurDAO GetInstance()
+        {
+            if (uneInstance == null) uneInstance = new UtilisateurDAO();
+            return uneInstance;
+        }
+    }
+}
