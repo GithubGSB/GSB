eb7c05f097013121f3c2d57bb97b25f0900ab2f9
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 17:00:05 +0200

    Ajout fonctionnalite

---
+++ ControleStockBO/Fonctionnalite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockBO
+{
+    public class Fonctionnalite
+    {
+        private string code;
+        private string libelle;
+
+        public Fonctionnalite(string code, string libelle)
+        {
+            this.code = code;
+            this.libelle = libelle;
+        }
+
+        public string Code { get => code; set => code = value; }
+        public string Libelle { get => libelle; set => libelle = value; }
+    }
+}
