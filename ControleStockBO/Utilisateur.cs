c5d9692d3a2f7123d4a25a0fea99c92c43eeccfd
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 17:03:50 +0200

    Ajout utilisateur

---
+++ ControleStockBO/Utilisateur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockBO
+{
+    public class Utilisateur
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private Profil profil;
+
+        public Utilisateur(int id, string nom, string prenom, Profil profil)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.profil = profil;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public Profil Profil { get => profil; set => profil = value; }
+    }
+}
