cbf7e52e407b8decd2368c1601f6595b9f2861ee
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 17:03:01 +0200

    Ajout de profil

---
+++ ControleStockBO/Profil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockBO
+{
+    public class Profil
+    {
+        private int id;
+        private string libelle;
+        private List<Fonctionnalite> lesFoncAutorises;
+
+        public Profil(int id, string libelle)
+        {
+            this.id = id;
+            this.libelle = libelle;
+            this.lesFoncAutorises = new List<Fonctionnalite>();
+        }
+
+        public Profil(int id, string libelle, List<Fonctionnalite> lesFoncAutorises)
+        {
+            this.id = id;
+            this.libelle = libelle;
+            this.lesFoncAutorises = lesFoncAutorises;
+        }
+
+        public int Id { get => id;}
+        public string Libelle { get => libelle; set => libelle = value; }
+        public List<Fonctionnalite> LesFoncAutorises { get => lesFoncAutorises; set => lesFoncAutorises = value; }
+    }
+}
