34a8b304c70d3420467e7dbdc732151a1351620c
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 21:28:43 +0200

    Ajout ProfilManager

---
+++ ControleStockBLL/ProfilManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockBLL
+{
+    /// <summary>
+    /// Classe permettant la gestion des profils
+    /// </summary>
+    public class ProfilManager
+    {
+        private static ProfilManager uneInstance;
+        /// <summary>
+        /// Premet de récupéré l'instance ProfilManager
+        /// </summary>
+        /// <returns>Instance de ProfilManager</returns>
+        public static ProfilManager GetInstance()
+        {
+            if (uneInstance == null) uneInstance = new ProfilManager();
+            return uneInstance;
+        }
+        private ProfilManager() { }
+    }
+}
