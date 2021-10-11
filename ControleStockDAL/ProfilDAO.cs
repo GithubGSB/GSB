f13cf90278c46dadae44850efea97e74204bedc3
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-11 17:12:19 +0200

    ajout de ProfilDAO

---
+++ ControleStockDAL/ProfilDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockDAL
+{
+    /// <summary>
+    /// Classe permettant l'accès aux données relatif au profil
+    /// </summary>
+    public class ProfilDAO
+    {
+        private static ProfilDAO uneInstance;
+        /// <summary>
+        /// Premet de récupéré l'instance ProfilDAO
+        /// </summary>
+        /// <returns>Instance de profilDAO</returns>
+        public static ProfilDAO GetInstance()
+        {
+            if (uneInstance == null) uneInstance = new ProfilDAO();
+            return uneInstance;
+        }
+    }
+}
