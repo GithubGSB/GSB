aa357461f71e0be134eb71d6475364775cd6ec24
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-12 09:20:21 +0200

    mise à jour GUI

--- GSBControleStockage/FormAjoutUtilisateur.Designer.cs
+++ GSBControleStockage/FormAjoutUtilisateur.Designer.cs
@@ -30,7 +30,6 @@ namespace GSBControleStockage
         private void InitializeComponent()
         {
             this.btnAjouter = new System.Windows.Forms.Button();
-            this.tabpnlPrincipal = new System.Windows.Forms.TableLayoutPanel();
             this.txtMdpConf = new System.Windows.Forms.TextBox();
             this.lblMdpConf = new System.Windows.Forms.Label();
             this.txtMdp = new System.Windows.Forms.TextBox();
@@ -42,67 +41,34 @@ namespace GSBControleStockage
             this.txtNom = new System.Windows.Forms.TextBox();
             this.cbxProfil = new System.Windows.Forms.ComboBox();
             this.btnAnnuler = new System.Windows.Forms.Button();
-            this.tabpnlPrincipal.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAjouter
             // 
-            this.btnAjouter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnAjouter.Location = new System.Drawing.Point(403, 373);
+            this.btnAjouter.Location = new System.Drawing.Point(295, 144);
             this.btnAjouter.Name = "btnAjouter";
-            this.btnAjouter.Size = new System.Drawing.Size(394, 74);
+            this.btnAjouter.Size = new System.Drawing.Size(275, 29);
             this.btnAjouter.TabIndex = 11;
             this.btnAjouter.Text = "Ajouter";
             this.btnAjouter.UseVisualStyleBackColor = true;
             this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
             // 
-            // tabpnlPrincipal
-            // 
-            this.tabpnlPrincipal.ColumnCount = 2;
-            this.tabpnlPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tabpnlPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tabpnlPrincipal.Controls.Add(this.btnAjouter, 1, 5);
-            this.tabpnlPrincipal.Controls.Add(this.txtMdpConf, 1, 4);
-            this.tabpnlPrincipal.Controls.Add(this.lblMdpConf, 0, 4);
-            this.tabpnlPrincipal.Controls.Add(this.txtMdp, 1, 3);
-            this.tabpnlPrincipal.Controls.Add(this.lblMdp, 0, 3);
-            this.tabpnlPrincipal.Controls.Add(this.lblProfil, 0, 2);
-            this.tabpnlPrincipal.Controls.Add(this.txtPrenom, 1, 1);
-            this.tabpnlPrincipal.Controls.Add(this.lblPrenom, 0, 1);
-            this.tabpnlPrincipal.Controls.Add(this.lblNom, 0, 0);
-            this.tabpnlPrincipal.Controls.Add(this.txtNom, 1, 0);
-            this.tabpnlPrincipal.Controls.Add(this.cbxProfil, 1, 2);
-            this.tabpnlPrincipal.Controls.Add(this.btnAnnuler, 0, 5);
-            this.tabpnlPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabpnlPrincipal.Location = new System.Drawing.Point(0, 0);
-            this.tabpnlPrincipal.Name = "tabpnlPrincipal";
-            this.tabpnlPrincipal.RowCount = 6;
-            this.tabpnlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tabpnlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tabpnlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tabpnlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tabpnlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tabpnlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tabpnlPrincipal.Size = new System.Drawing.Size(800, 450);
-            this.tabpnlPrincipal.TabIndex = 1;
-            // 
             // txtMdpConf
             // 
             this.txtMdpConf.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtMdpConf.Location = new System.Drawing.Point(403, 322);
+            this.txtMdpConf.Location = new System.Drawing.Point(295, 119);
             this.txtMdpConf.Name = "txtMdpConf";
             this.txtMdpConf.PasswordChar = '*';
-            this.txtMdpConf.Size = new System.Drawing.Size(394, 22);
+            this.txtMdpConf.Size = new System.Drawing.Size(275, 22);
             this.txtMdpConf.TabIndex = 9;
             this.txtMdpConf.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblMdpConf
             // 
             this.lblMdpConf.AutoSize = true;
-            this.lblMdpConf.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblMdpConf.Location = new System.Drawing.Point(3, 296);
+            this.lblMdpConf.Location = new System.Drawing.Point(97, 119);
             this.lblMdpConf.Name = "lblMdpConf";
-            this.lblMdpConf.Size = new System.Drawing.Size(394, 74);
+            this.lblMdpConf.Size = new System.Drawing.Size(192, 17);
             this.lblMdpConf.TabIndex = 8;
             this.lblMdpConf.Text = "Mot de passe (confirmation) :";
             this.lblMdpConf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -110,20 +76,19 @@ namespace GSBControleStockage
             // txtMdp
             // 
             this.txtMdp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtMdp.Location = new System.Drawing.Point(403, 248);
+            this.txtMdp.Location = new System.Drawing.Point(295, 90);
             this.txtMdp.Name = "txtMdp";
             this.txtMdp.PasswordChar = '*';
-            this.txtMdp.Size = new System.Drawing.Size(394, 22);
+            this.txtMdp.Size = new System.Drawing.Size(275, 22);
             this.txtMdp.TabIndex = 7;
             this.txtMdp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblMdp
             // 
             this.lblMdp.AutoSize = true;
-            this.lblMdp.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblMdp.Location = new System.Drawing.Point(3, 222);
+            this.lblMdp.Location = new System.Drawing.Point(188, 93);
             this.lblMdp.Name = "lblMdp";
-            this.lblMdp.Size = new System.Drawing.Size(394, 74);
+            this.lblMdp.Size = new System.Drawing.Size(101, 17);
             this.lblMdp.TabIndex = 6;
             this.lblMdp.Text = "Mot de passe :";
             this.lblMdp.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -131,10 +96,9 @@ namespace GSBControleStockage
             // lblProfil
             // 
             this.lblProfil.AutoSize = true;
-            this.lblProfil.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblProfil.Location = new System.Drawing.Point(3, 148);
+            this.lblProfil.Location = new System.Drawing.Point(241, 65);
             this.lblProfil.Name = "lblProfil";
-            this.lblProfil.Size = new System.Drawing.Size(394, 74);
+            this.lblProfil.Size = new System.Drawing.Size(48, 17);
             this.lblProfil.TabIndex = 4;
             this.lblProfil.Text = "Profil :";
             this.lblProfil.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -142,19 +106,18 @@ namespace GSBControleStockage
             // txtPrenom
             // 
             this.txtPrenom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtPrenom.Location = new System.Drawing.Point(403, 100);
+            this.txtPrenom.Location = new System.Drawing.Point(295, 33);
             this.txtPrenom.Name = "txtPrenom";
-            this.txtPrenom.Size = new System.Drawing.Size(394, 22);
+            this.txtPrenom.Size = new System.Drawing.Size(275, 22);
             this.txtPrenom.TabIndex = 3;
             this.txtPrenom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblPrenom
             // 
             this.lblPrenom.AutoSize = true;
-            this.lblPrenom.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblPrenom.Location = new System.Drawing.Point(3, 74);
+            this.lblPrenom.Location = new System.Drawing.Point(224, 35);
             this.lblPrenom.Name = "lblPrenom";
-            this.lblPrenom.Size = new System.Drawing.Size(394, 74);
+            this.lblPrenom.Size = new System.Drawing.Size(65, 17);
             this.lblPrenom.TabIndex = 2;
             this.lblPrenom.Text = "Prénom :";
             this.lblPrenom.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -162,20 +125,19 @@ namespace GSBControleStockage
             // lblNom
             // 
             this.lblNom.AutoSize = true;
-            this.lblNom.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblNom.Location = new System.Drawing.Point(3, 0);
+            this.lblNom.Location = new System.Drawing.Point(244, 7);
             this.lblNom.Name = "lblNom";
-            this.lblNom.Size = new System.Drawing.Size(394, 74);
+            this.lblNom.Size = new System.Drawing.Size(45, 17);
             this.lblNom.TabIndex = 0;
             this.lblNom.Text = "Nom :";
             this.lblNom.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // txtNom
             // 
-            this.txtNom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNom.Location = new System.Drawing.Point(403, 26);
+            this.txtNom.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtNom.Location = new System.Drawing.Point(295, 5);
             this.txtNom.Name = "txtNom";
-            this.txtNom.Size = new System.Drawing.Size(394, 22);
+            this.txtNom.Size = new System.Drawing.Size(275, 22);
             this.txtNom.TabIndex = 1;
             this.txtNom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -183,17 +145,16 @@ namespace GSBControleStockage
             // 
             this.cbxProfil.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.cbxProfil.FormattingEnabled = true;
-            this.cbxProfil.Location = new System.Drawing.Point(403, 173);
+            this.cbxProfil.Location = new System.Drawing.Point(295, 61);
             this.cbxProfil.Name = "cbxProfil";
-            this.cbxProfil.Size = new System.Drawing.Size(394, 24);
+            this.cbxProfil.Size = new System.Drawing.Size(275, 24);
             this.cbxProfil.TabIndex = 5;
             // 
             // btnAnnuler
             // 
-            this.btnAnnuler.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnAnnuler.Location = new System.Drawing.Point(3, 373);
+            this.btnAnnuler.Location = new System.Drawing.Point(15, 144);
             this.btnAnnuler.Name = "btnAnnuler";
-            this.btnAnnuler.Size = new System.Drawing.Size(394, 74);
+            this.btnAnnuler.Size = new System.Drawing.Size(274, 29);
             this.btnAnnuler.TabIndex = 10;
             this.btnAnnuler.Text = "Annuler";
             this.btnAnnuler.UseVisualStyleBackColor = true;
@@ -203,20 +164,29 @@ namespace GSBControleStockage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tabpnlPrincipal);
+            this.ClientSize = new System.Drawing.Size(582, 185);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.txtMdpConf);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.txtMdp);
+            this.Controls.Add(this.lblMdpConf);
+            this.Controls.Add(this.cbxProfil);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.lblMdp);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.lblPrenom);
+            this.Controls.Add(this.lblProfil);
             this.Name = "FormAjoutUtilisateur";
             this.Text = "Ajout d\'un utilisateur";
-            this.tabpnlPrincipal.ResumeLayout(false);
-            this.tabpnlPrincipal.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnAjouter;
-        private System.Windows.Forms.TableLayoutPanel tabpnlPrincipal;
         private System.Windows.Forms.TextBox txtMdpConf;
         private System.Windows.Forms.Label lblMdpConf;
         private System.Windows.Forms.TextBox txtMdp;
