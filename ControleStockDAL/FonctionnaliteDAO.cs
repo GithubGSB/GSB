d3eff1095a6497260c081dc20b0537e080f6110c
Rambaud Evan <evan.rambaud@gmail.com>
2021-10-13 11:18:51 +0200

    ajout de la DAO de fonctionnalite

---
+++ ControleStockDAL/FonctionnaliteDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleStockDAL
+{
+    /// <summary>
+    /// Classe permettant l'accès aux données relatif au fonctionnalité
+    /// </summary>
+    public class FonctionnaliteDAO
+    {
+        private static FonctionnaliteDAO uneInstance;
+        /// <summary>
+        /// Premet de récupéré l'instance FonctionnaliteDAO
+        /// </summary>
+        /// <returns>Instance de FonctionnaliteDAO</returns>
+        public static FonctionnaliteDAO GetInstance()
+        {
+            if (uneInstance == null) uneInstance = new FonctionnaliteDAO();
+            return uneInstance;
+        }
+        private FonctionnaliteDAO() { }
+    }
+}
