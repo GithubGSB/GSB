635bc999d04048b7be5269e0df311dd4ba8961cb
congiu <congiu.nicolas@outlook.com>
2021-10-21 12:06:08 +0200

    #Mise en place de la modif#

---
+++ GSBControleStockage/FormModifZoneStockage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ControleStockBLL;
+using ControleStockBO;
+
+namespace GSBControleStockage
+{
+    public partial class FormModifZoneStockage : Form
+    {
+        public FormModifZoneStockage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnModifZS_Click(object sender, EventArgs e)
+        {
+            ZoneStockageManager.GetInstance().ModifZoneStockage();
+        }
+
+        
+    }
+}
